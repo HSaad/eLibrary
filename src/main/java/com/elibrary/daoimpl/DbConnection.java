f45d8dc37509774fa409392938bca8aa92664cef
HSaad <hagrsaad16@gmail.com>
2020-01-18 14:38:28 -0500

    Fixed Database connection

--- src/main/java/com/elibrary/daoimpl/DbConnection.java
+++ src/main/java/com/elibrary/daoimpl/DbConnection.java
@@ -1,14 +1,47 @@
 package com.elibrary.daoimpl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DbConnection {
-	private static final String JPA_UNIT_NAME = "ELibrary";
+	private static final String JPA_UNIT_NAME = "DATABASE_URL";
 	private static DbConnection connection = null;
 	private EntityManagerFactory emf;
-	
+//	URI dbUri = null;
+//	String username = dbUri.getUserInfo().split(":")[0];
+//	String password = dbUri.getUserInfo().split(":")[1];
+//	String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+//            + dbUri.getPort() + dbUri.getPath()
+//            + "?sslmode=require";
+//	
+//    private static Connection getConnection() throws URISyntaxException, SQLException {
+//        URI dbUri = null;
+//        if(System.getenv("DATABASE_URL") != null) {
+//            dbUri = new URI(System.getenv("DATABASE_URL"));
+//        }else {
+//            String DATABASE_URL = "postgres://ubuntu:ubuntu@localhost:5432/userdb";
+//            dbUri = new URI(DATABASE_URL);
+//        }
+//
+//		String username = dbUri.getUserInfo().split(":")[0];
+//		String password = dbUri.getUserInfo().split(":")[1];
+//		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+//                + dbUri.getPort() + dbUri.getPath()
+//                + "?sslmode=require";
+//        /*Connection connection = DriverManager.getConnection(
+//                "jdbc:postgresql://localhost:5432/userdb?sslmode=require",
+//                "ubuntu",
+//                "ubuntu");*/
+//		return DriverManager.getConnection(dbUrl, username, password);
+//	}
+//	
 	private DbConnection() {
 		init();
 	}
