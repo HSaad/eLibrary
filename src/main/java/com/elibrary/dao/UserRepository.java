5bdbc0371c185a7369c672212ef440a8d0edc5fa
HSaad <hagrsaad16@gmail.com>
2020-01-19 15:41:16 -0500

    Add Repository tag to userRepository file

--- src/main/java/com/elibrary/dao/UserRepository.java
+++ src/main/java/com/elibrary/dao/UserRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.persistence.NamedQuery;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 
 	List<User> findAllOrderedByNameDescending();
