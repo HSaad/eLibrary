d375d23dc7bf09ec2512f6e1d185f5263665b386
HSaad <hagrsaad16@gmail.com>
2020-01-19 17:44:39 -0500

    Change data sample

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO cuser(firstName, lastName, email, username, pw) VALUES('John', 'Smith', 'john.smith@gmail.com', 'jsmith', '123');
-INSERT INTO cuser(firstName, lastName, email, username, pw) VALUES('Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
+INSERT INTO cuser(first_name, last_name, email, username, pw) VALUES('John', 'Smith', 'john.smith@gmail.com', 'jsmith', '123');
+INSERT INTO cuser(first_name, last_name, email, username, pw) VALUES('Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
