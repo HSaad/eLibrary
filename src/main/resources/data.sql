c72db3b5cf03569b8c59ce9088d30c70c356ee1d
HSaad <hagrsaad16@gmail.com>
2020-01-19 15:01:09 -0500

    Fix sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO cuser(first, last, email, company, city) VALUES('John', 'Smith', 'john.smith@gmail.com', 'John Inc' 'Toronto');
-INSERT INTO cuser(first, last, email, company, city) VALUES('Jane', 'Smith', 'jane.smith@gmail.com', 'Jane Inc' 'Toronto');
+INSERT INTO cuser(first, last, email, company, city) VALUES('John', 'Smith', 'john.smith@gmail.com', 'John Inc', 'Toronto');
+INSERT INTO cuser(first, last, email, company, city) VALUES('Jane', 'Smith', 'jane.smith@gmail.com', 'Jane Inc', 'Toronto');
