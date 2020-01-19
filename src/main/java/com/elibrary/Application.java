3bac4ee08754b731c79290eb41bb11cc62789dd6
HSaad <hagrsaad16@gmail.com>
2020-01-19 15:48:57 -0500

    Add enableRepo to Spring Application

--- src/main/java/com/elibrary/Application.java
+++ src/main/java/com/elibrary/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.*;
 
 import java.sql.*;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.elibrary.dao")
 public class Application{
 
     public static void main(String[] args) {
