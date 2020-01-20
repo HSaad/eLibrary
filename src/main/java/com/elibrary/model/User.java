015be08f61d8d6962330b82d0b9b07bb64424583
HSaad <hagrsaad16@gmail.com>
2020-01-19 19:35:10 -0500

    Change Id generation in User

--- src/main/java/com/elibrary/model/User.java
+++ src/main/java/com/elibrary/model/User.java
@@ -35,7 +35,7 @@ import javax.persistence.Table;
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 	private String firstName;
