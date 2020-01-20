cce12c5b430930b120b5773dc77d6f1d06d069af
HSaad <hagrsaad16@gmail.com>
2020-01-19 23:08:19 -0500

    Change schema and sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO cuser(first_name, last_name, email, username, pw) VALUES('John', 'Smith', 'john.smith@gmail.com', 'jsmith', '123');
-INSERT INTO cuser(first_name, last_name, email, username, pw) VALUES('Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'John', 'Smith', 'john.smith@gmail.com', 'jsmith', '123');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS cuser
 (
      id SERIAL PRIMARY KEY NOT NULL,
+     user_type varchar(20),
      first_name varchar(100),
      last_name varchar (100),
      email varchar (100),
      username varchar (150),
      pw varchar(100),
-     user_type varchar(20),
      privilege varchar(100)
 );
