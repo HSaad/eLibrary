4fbbb5175a80248d23432dd6602551418d0e4292
HSaad <hagrsaad16@gmail.com>
2020-01-20 14:12:28 -0500

    Remove findAllType

--- src/main/java/com/elibrary/model/LibraryItem.java
+++ src/main/java/com/elibrary/model/LibraryItem.java
@@ -27,15 +27,14 @@ import javax.persistence.Table;
 	@NamedQuery(name = "Library.findByCreator", query = "SELECT i FROM LibraryItem i WHERE i.creator = :creator"),
 	//find all library items that are available (true)
 	@NamedQuery(name = "Library.findByAvailable", query = "SELECT i FROM LibraryItem i WHERE i.available = :available"),
-	
-	//find all ebooks
-	@NamedQuery(name = "Library.findAllEbooks", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Ebook"),
-	//find all audiobooks
-	@NamedQuery(name = "Library.findAllAudioBooks", query = "SELECT i from LibraryItem i WHERE TYPE(i) = AudioBook"),
-	//find all magazines
-	@NamedQuery(name = "Library.findAllMagazines", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Magazine"),
-	//find all videos
-	@NamedQuery(name = "Library.findAllVideos", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Video"),
+//	//find all ebooks
+//	@NamedQuery(name = "Library.findAllEbooks", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Ebook"),
+//	//find all audiobooks
+//	@NamedQuery(name = "Library.findAllAudioBooks", query = "SELECT i from LibraryItem i WHERE TYPE(i) = AudioBook"),
+//	//find all magazines
+//	@NamedQuery(name = "Library.findAllMagazines", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Magazine"),
+//	//find all videos
+//	@NamedQuery(name = "Library.findAllVideos", query = "SELECT i from LibraryItem i WHERE TYPE(i) = Video"),
 	//Find library items by type
 	@NamedQuery(name = "Library.findByType", query = "SELECT i from LibraryItem i WHERE TYPE(i) = :type")
 })
