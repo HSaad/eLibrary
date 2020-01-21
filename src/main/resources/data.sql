949fe7d983e0ba9d4bcf5425167d388f391b6643
HSaad <hagrsaad16@gmail.com>
2020-01-20 19:43:55 -0500

    Add sample data for laon

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES(
 INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
 INSERT INTO libitem(item_type, title, creator, description, genre, publication_year, available, file_size) VALUES('Book', 'Harry Potter', 'JK Rowling', 'Harry yer a wizard', 'Fantasy', 1999, 'true', 150);
 
+INSERT INTO loan(item_id, user_id) VALUES(1, 1);
