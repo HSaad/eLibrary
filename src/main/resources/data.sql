e3ed72a41217a991339ffc91cf6735d3e58350d4
HSaad <hagrsaad16@gmail.com>
2020-01-21 09:42:32 -0500

    Add moreuser data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,15 @@
-INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'John', 'Smith', 'john.smith@gmail.com', 'jsmith', '123');
-INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Borrower', 'John', 'Smith', 'john.smith@gmail.com', 'jsmith', '1234567');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Borrower', 'Jane', 'Smith', 'jane.smith@gmail.com', 'janesmith', '1234567');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Borrower', 'Hagr', 'Saad', 'hagr@gmail.com', 'hsaad', '1234567');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Borrower', 'Johnathan', 'Smith', 'jonsmith@gmail.com', 'jonathan', '1234567');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Admin', 'Hagr', 'Saad', 'hagrsaad@gmail.com', 'hagr', '12345');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Librarian', 'Joe', 'Brown', 'helloworld@gmail.com', 'joe', '12345');
+INSERT INTO cuser(user_type, first_name, last_name, email, username, pw) VALUES('Librarian', 'Jack', 'Brown', 'jack.brown@gmail.com', 'jack', '12345');
+
+
 INSERT INTO libitem(item_type, title, creator, description, genre, publication_year, available, file_size) VALUES('Book', 'Harry Potter', 'JK Rowling', 'Harry yer a wizard', 'Fantasy', 1999, 'true', 150);
 
 INSERT INTO loan(item_id, user_id) VALUES(1, 1);
+
+
+
